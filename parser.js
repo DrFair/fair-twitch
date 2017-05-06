ce4443b227aaf7e0a8e872e65618bb9cd7e1a560
Mads Skovgaard <madsskovgaard@hotmail.com>
2017-05-06 21:28:19 +0200

    Beautifies chat user parser and removes channel prefix.

--- parser.js
+++ parser.js
@@ -1,21 +1,28 @@
 exports.createUser = function(args, tags) {
     let user = {};
-    let argsSplit = args[0].split(":");
+
+    let argsSplit = args[0].split(':');
     if (argsSplit.length > 1) user.msg = argsSplit[1];
-    else user.msg = "";
-    let channelSplit = argsSplit[0].split(" ");
+    else user.msg = '';
+
+    let channelSplit = argsSplit[0].split(' ');
     if (channelSplit.length > 2) user.channel = channelSplit[2];
-    user.display_name = tags["display-name"];
-    user.user_id = Number(tags["user-id"]);
-    user.mod = Number(tags["mod"]) === 1;
-    user.subscriber = Number(tags["subscriber"]) === 1;
-    user.turbo = Number(tags["turbo"]) === 1;
-    user.user_type = tags["user-type"];
-    if (tags["login"]) user.login = tags["login"];
-    if (tags["color"]) user.color = tags["color"];
-    if (tags["badges"]) user.badges = tags["badges"];
-    if (tags["emotes"]) user.emotes = tags["emotes"];
-    if (tags["bits"]) user.bits = tags["bits"];
-    if (tags["message"]) user.message = tags["message"];
+    if (user.channel && user.channel.charAt(0) === '#') user.channel = user.channel.substr(1);
+
+    user.display_name = tags['display-name'];
+    user.user_id = Number(tags['user-id']);
+    user.mod = Number(tags['mod']) === 1;
+    user.subscriber = Number(tags['subscriber']) === 1;
+    user.turbo = Number(tags['turbo']) === 1;
+    user.user_type = tags['user-type'];
+
+    // Sometimes these tags are present
+    if (tags['login']) user.login = tags['login'];
+    if (tags['color']) user.color = tags['color'];
+    if (tags['badges']) user.badges = tags['badges'];
+    if (tags['emotes']) user.emotes = tags['emotes'];
+    if (tags['bits']) user.bits = tags['bits'];
+    if (tags['message']) user.message = tags['message'];
+
     return user;
 };
