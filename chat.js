37f7174ef5f6e6fd6c21403b3e2dca2b712d4336
Mads Skovgaard <madsskovgaard@hotmail.com>
2017-12-02 18:13:15 +0100

    Reworks irc error handling to always handle them even if there's no listeners.

--- chat.js
+++ chat.js
@@ -56,6 +56,15 @@ function TwitchBot(options, channels) {
     self.irc.send('CAP REQ', 'twitch.tv/tags');
     self.irc.send('CAP REQ', 'twitch.tv/commands');
 
+
+    self.irc.addListener('error', function (err) {
+        // Twitch can't handle WHOIS requests, but there's no way to disable those in node-irc
+        if (err['rawCommand'] === '421' && err['args'] && err['args'][1] === 'WHOIS') {
+            return;
+        }
+        submitError(self.errorListeners, err);
+    });
+
     // Used to store callbacks for when connection was successful
     self.connectedCallbacks = [];
 
@@ -126,13 +135,6 @@ TwitchBot.prototype.setOptions = function (options) {
 
 // Callback: err
 TwitchBot.prototype.onError = function (callback) {
-    this.irc.addListener('error', function (err) {
-        // Twitch can't handle WHOIS requests, but there's no way to disable those in node-irc
-        if (err['rawCommand'] === '421' && err['args'] && err['args'][1] === 'WHOIS') {
-            return;
-        }
-        callback(err);
-    });
     this.errorListeners.push(callback);
 };
 
