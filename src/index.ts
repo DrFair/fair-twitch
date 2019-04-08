7c80f8003c58a6a545453f4d6b4c5a4436b6e1b3
Mads Skovgaard <madsskovgaard@hotmail.com>
2019-04-08 22:57:19 +0200

    Moves chat index to parent folder.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 import * as request from 'request';
 
-class TwitchAPI {
-  options: any;
+import TwitchIRC from './chat';
 
-  constructor(options: any) {
-    this.options = options;
-  }
-
-  test(callback?: (err: any, res: request.Response, body: any) => void): void {
-    request('http://www.google.com', (err, res, body) => {
-      if (callback) callback(err, res, body);
-    });
-  }
+export default {
+  TwitchIRC,
 }
-
-export default TwitchAPI;
