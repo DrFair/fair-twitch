405bea46552b642a66ae04c89fde3eb4bc645031
Mads Skovgaard <madsskovgaard@hotmail.com>
2019-04-09 23:09:10 +0200

    Fixes console output when joining channel.

--- src/chat.ts
+++ src/chat.ts
@@ -532,12 +532,9 @@ class TwitchIRC extends EventEmitter {
     if (!channel.startsWith('#')) channel = '#' + channel;
     this.send('JOIN ' + channel);
     if (typeof (callback) === 'function') {
-      console.log('HELLO1');
       this.onceIf('join', (chn) => {
         if (!chn.startsWith('#')) chn = '#' + chn;
-        console.log('HELLO2 ' + chn);
         if (chn === channel) {
-          console.log('HELLO3');
           callback();
           return true;
         }
