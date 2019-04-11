82dbe7c4c675cfe3f97b443fe51ca7363601102e
Mads Skovgaard <madsskovgaard@hotmail.com>
2019-04-11 19:01:35 +0200

    Changes basic chat tests.

--- test/chat.test.ts
+++ test/chat.test.ts
@@ -1,42 +1,120 @@
 import TwitchIRC from '../src/chat';
-import { expect } from 'chai';
 import 'mocha';
 
-describe('Chat', () => {
-  const chat = new TwitchIRC();
-  let innerDone = null;
-  chat.on('error', (err) => {
-    chat.removeAllListeners();
-    if (innerDone) innerDone(err);
-  })
+describe('Chat', function() {
+  this.slow(1000);
+  this.timeout(5000);
 
-  it('Should connect properly', (done) => {
-    innerDone = done;
-    chat.once('ready', () => {
+  describe('Create, auto connect, ready, close', () => {
+    let chat: TwitchIRC;
+    let innerDone = null;
+
+    it('Create', (done) => {
+      chat = new TwitchIRC();
+      chat.on('error', (err) => {
+        chat.removeAllListeners();
+        if (innerDone) innerDone(err);
+      });
       done();
+    });
+
+    it('Connect, ready', (done) => {
+      innerDone = done;
+      chat.once('ready', () => {
+        done();
+      });
+    });
+
+    it('Close', (done) => {
+      innerDone = done;
+      chat.close((err) => {
+        if (err) done(err);
+        else done();
+        chat.removeAllListeners();
+      });
     });
   });
 
-  it('Should join "twitch" channel properly', (done) => {
-    innerDone = done;
-    chat.join('twitch', () => {
+  describe('Create, connect, ready, close', () => {
+    let chat: TwitchIRC;
+    let innerDone = null;
+
+    it('Create', (done) => {
+      chat = new TwitchIRC({
+        autoConnect: false
+      });
+      chat.on('error', (err) => {
+        chat.removeAllListeners();
+        if (innerDone) innerDone(err);
+      });
       done();
+    });
+
+    it('Connect', (done) => {
+      innerDone = done;
+      chat.connect(() => {
+        done();
+      });
+    });
+
+    it('Ready', (done) => {
+      innerDone = done;
+      chat.once('ready', () => {
+        done();
+      });
+    });
+
+    it('Close', (done) => {
+      innerDone = done;
+      chat.close((err) => {
+        if (err) done(err);
+        else done();
+        chat.removeAllListeners();
+      });
     });
   });
 
-  it('Should part "twitch" channel properly', (done) => {
-    innerDone = done;
-    chat.part('twitch', () => {
+  describe('Connect, ready, join channel, close', () => {
+    let chat: TwitchIRC;
+    let innerDone = null;
+
+    it('Create', (done) => {
+      chat = new TwitchIRC();
+      chat.on('error', (err) => {
+        chat.removeAllListeners();
+        if (innerDone) innerDone(err);
+      });
       done();
     });
-  });
 
-  it('Should close connection properly', (done) => {
-    innerDone = done;
-    chat.close((err) => {
-      if (err) done(err);
-      else done();
-      chat.removeAllListeners();
+    it('Connect, ready', (done) => {
+      innerDone = done;
+      chat.once('ready', () => {
+        done();
+      });
+    });
+
+    it('Join "twitch" channel', (done) => {
+      innerDone = done;
+      chat.join('twitch', () => {
+        done();
+      });
+    });
+
+    it('Part "twitch" channel', (done) => {
+      innerDone = done;
+      chat.part('twitch', () => {
+        done();
+      });
+    });
+
+    it('Close', (done) => {
+      innerDone = done;
+      chat.close((err) => {
+        if (err) done(err);
+        else done();
+        chat.removeAllListeners();
+      });
     });
   });
 
